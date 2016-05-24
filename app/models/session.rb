6bea0925f50d5c0386b39594befda28c702a0c9b
Arcane52689 <tjenkins52689@gmail.com>
2016-05-24 11:32:36 -0400

    add sessions

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,16 @@
 class Session < ActiveRecord::Base
   belongs_to :user
+
+  before_save :ensure_unique_token
+
+
+
+
+  def ensure_unique_token
+    token = SecureRandom.urlsafe_base64(16)
+    while Session..exists?(token: token)
+      token = SecureRandom.urlsafe_base64(16)
+    end
+  end
+
 end
